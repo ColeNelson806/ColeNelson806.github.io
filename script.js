2e89b519f567a3d7aa2965f3c1d1b42a080debde
ColeNelson806 <128399411+ColeNelson806@users.noreply.github.com>
2023-05-03 15:22:11 -0500

    Created script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  const btn = document.getElementById("submit");
+  btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.alert("Thank you for signing up!");
+    window.location.href = "https://colenelson806.github.io/";
+  });
+};
